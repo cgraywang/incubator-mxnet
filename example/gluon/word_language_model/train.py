59f733ca47f53abbbf734f2b94c806943cade567
Wang <wangcg.pku@gmail.com>
2018-02-16 16:57:44 -0800

    update train with default params

--- example/gluon/word_language_model/train.py
+++ example/gluon/word_language_model/train.py
@@ -38,7 +38,7 @@ parser.add_argument('--lr', type=float, default=30,
                     help='initial learning rate')
 parser.add_argument('--clip', type=float, default=0.25,
                     help='gradient clipping')
-parser.add_argument('--epochs', type=int, default=40,
+parser.add_argument('--epochs', type=int, default=750,
                     help='upper epoch limit')
 parser.add_argument('--batch_size', type=int, default=80, metavar='N',
                     help='batch size')
