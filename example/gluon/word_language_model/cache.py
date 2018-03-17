237036ed19ee633b525017f9ce633af5bb884351
Wang <chgwang@8c8590431a79.ant.amazon.com>
2018-03-16 17:41:13 -0700

    updata cache

--- example/gluon/word_language_model/cache.py
+++ example/gluon/word_language_model/cache.py
@@ -83,19 +83,15 @@ args = parser.parse_args()
 context = [mx.cpu()] if args.gpus is None or args.gpus == "" else [
         mx.gpu(int(i)) for i in args.gpus.split(',')]
 
+
 train_dataset = contrib.data.text.WikiText2('./data', 'train', seq_len=args.bptt)
 vocab = train_dataset.vocabulary
+
 val_dataset, test_dataset = [contrib.data.text.WikiText2('./data', segment,
                                                          vocab=vocab,
                                                          seq_len=args.bptt)
                              for segment in ['validation', 'test']]
 
-nbatch_train = len(train_dataset) // args.batch_size
-train_data = gluon.data.DataLoader(train_dataset,
-                                   batch_size=args.batch_size,
-                                   sampler=contrib.data.IntervalSampler(len(train_dataset),
-                                                                        nbatch_train),
-                                   last_batch='discard')
 
 nbatch_val = len(val_dataset) // args.batch_size
 val_data = gluon.data.DataLoader(val_dataset,
@@ -111,14 +107,9 @@ test_data = gluon.data.DataLoader(test_dataset,
                                                                        nbatch_test),
                                   last_batch='discard')
 
-
 ###############################################################################
-# Build the model
+# Build model
 ###############################################################################
-
-
-ntokens = len(vocab)
-
 if args.weight_dropout:
     model = AWDLSTM(args.model, vocab, args.emsize, args.nhid, args.nlayers,
                  args.dropout, args.dropout_h, args.dropout_i, args.dropout_e, args.weight_dropout,
@@ -126,21 +117,8 @@ if args.weight_dropout:
 else:
     model = RNNModel(args.model, vocab, args.emsize, args.nhid,
                  args.nlayers, args.dropout, args.tied)
-    
+
 model.collect_params().initialize(mx.init.Xavier(), ctx=context)
-
-
-compression_params = None if args.gctype == 'none' else {'type': args.gctype, 'threshold': args.gcthreshold}
-trainer = gluon.Trainer(model.collect_params(), 'sgd',
-                        {'learning_rate': args.lr,
-                         'momentum': 0,
-                         'wd': 0},
-                        compression_params=compression_params)
-loss = gluon.loss.SoftmaxCrossEntropyLoss()
-
-###############################################################################
-# Training code
-###############################################################################
 
 def detach(hidden):
     if isinstance(hidden, (tuple, list)):
@@ -148,78 +126,9 @@ def detach(hidden):
     else:
         hidden = hidden.detach()
     return hidden
-
-def eval(data_source):
-    total_L = 0.0
-    ntotal = 0
-    hidden = model.begin_state(func=mx.nd.zeros, batch_size=args.batch_size, ctx=context[0])
-    for i, (data, target) in enumerate(data_source):
-        data = data.as_in_context(context[0]).T
-        target= target.as_in_context(context[0]).T
-        output, hidden = model(data, hidden)
-        L = loss(mx.nd.reshape(output, (-3, -1)),
-                 mx.nd.reshape(target, (-1,)))
-        total_L += mx.nd.sum(L).asscalar()
-        ntotal += L.size
-    return total_L / ntotal
-
-def train():
-    best_val = float("Inf")
-    start_train_time = time.time()
-    for epoch in range(args.epochs):
-        total_L = 0.0
-        start_epoch_time = time.time()
-        hiddens = [model.begin_state(func=mx.nd.zeros, batch_size=args.batch_size, ctx=ctx) for ctx in context]
-        for i, (data, target) in enumerate(train_data):
-            start_batch_time = time.time()
-            data = data.T
-            target= target.T
-            data_list = gluon.utils.split_and_load(data, context, even_split=False)
-            target_list = gluon.utils.split_and_load(target, context, even_split=False)
-            hiddens = [detach(hidden) for hidden in hiddens]
-            Ls = []
-            with autograd.record():
-                for i, (X, y, h) in enumerate(zip(data_list, target_list, hiddens)):
-                    output, h = model(X, h)
-                    Ls.append(loss(mx.nd.reshape(output, (-3, -1)), mx.nd.reshape(y, (-1,))))
-                    hiddens[i] = h
-            for L in Ls:
-                L.backward()
-            for ctx in context:
-                grads = [p.grad(ctx) for p in model.collect_params().values()]
-                gluon.utils.clip_global_norm(grads, args.clip * args.bptt * args.batch_size)
-            
-            trainer.step(args.batch_size)
-                
-            total_L += sum([mx.nd.sum(L).asscalar() for L in Ls])
-            
-            if i % args.log_interval == 0 and i > 0:
-                cur_L = total_L / args.bptt / args.batch_size / args.log_interval
-                print('[Epoch %d Batch %d] loss %.2f, ppl %.2f'%(
-                    epoch, i, cur_L, math.exp(cur_L)))
-                total_L = 0.0
-            
-            print('[Epoch %d Batch %d] throughput %.2f samples/s'%(
-                    epoch, i, args.batch_size / (time.time() - start_batch_time)))
-        
-        mx.nd.waitall()
-
-        print('[Epoch %d] throughput %.2f samples/s'%(
-                    epoch, (args.batch_size * nbatch_train) / (time.time() - start_epoch_time)))
-        val_L = eval(val_data)
-        print('[Epoch %d] time cost %.2fs, valid loss %.2f, valid ppl %.2f'%(
-            epoch, time.time()-start_epoch_time, val_L, math.exp(val_L)))
-
-        if val_L < best_val:
-            best_val = val_L
-            test_L = eval(test_data)
-            model.collect_params().save(args.save)
-            print('test loss %.2f, test ppl %.2f'%(test_L, math.exp(test_L)))
-            
-    print('Total training throughput %.2f samples/s'%(
-                            (args.batch_size * nbatch_train * args.epochs) / (time.time() - start_train_time)))
     
-def evalcache(datasource):
+#cache model on the fly
+def eval(datasource):
     total_L = 0
     hidden = model.begin_state(func=mx.nd.zeros, batch_size=args.batch_size, ctx=context[0])
     next_word_history = None
@@ -261,12 +170,10 @@ def evalcache(datasource):
     
 
 if __name__ == '__main__':
-    start_pipeline_time = time.time()
-    if not args.eval_only:
-        train()
+    #load pretrained model
     model.collect_params().load(args.save, context)
-    val_L = evalcache(val_data)
-    test_L = evalcache(test_data)
+    val_L = eval(val_data)
+    test_L = eval(test_data)
     print('Best validation loss %.2f, test ppl %.2f'%(val_L, math.exp(val_L)))
     print('Best test loss %.2f, test ppl %.2f'%(test_L, math.exp(test_L)))
     print('Total time cost %.2fs'%(time.time()-start_pipeline_time))
