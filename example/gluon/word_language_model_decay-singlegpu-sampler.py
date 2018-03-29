ce15958bcc1cb02affee10261d2803ad26dbe3a9
Wang <chgwang@8c8590431a79.ant.amazon.com>
2018-03-28 17:08:36 -0700

    update sampler

--- example/gluon/word_language_model_decay-singlegpu-sampler.py
+++ example/gluon/word_language_model_decay-singlegpu-sampler.py
@@ -96,22 +96,22 @@ val_dataset, test_dataset = [data.text.WikiText2(segment=segment,
 nbatch_train = len(train_dataset) // args.batch_size
 train_data = gluon.data.DataLoader(train_dataset.transform(index_tokens),
                                    batch_size=args.batch_size,
-                                   sampler=gluon.contrib.data.IntervalSampler(len(train_dataset),
-                                                                              nbatch_train*args.batch_size),
+                                   sampler=gluon.contrib.data.IntervalSampler(nbatch_train*args.batch_size,
+                                                                              nbatch_train),
                                    last_batch='discard')
 
 nbatch_val = len(val_dataset) // args.batch_size
 val_data = gluon.data.DataLoader(val_dataset.transform(index_tokens),
                                  batch_size=args.batch_size,
-                                 sampler=gluon.contrib.data.IntervalSampler(len(val_dataset),
-                                                                            nbatch_val*args.batch_size),
+                                 sampler=gluon.contrib.data.IntervalSampler(nbatch_val*args.batch_size,
+                                                                            nbatch_val),
                                  last_batch='discard')
 
 nbatch_test = len(test_dataset) // args.batch_size
 test_data = gluon.data.DataLoader(test_dataset.transform(index_tokens),
                                   batch_size=args.batch_size,
-                                  sampler=gluon.contrib.data.IntervalSampler(len(test_dataset),
-                                                                             nbatch_test*args.batch_size),
+                                  sampler=gluon.contrib.data.IntervalSampler(nbatch_test*args.batch_size,
+                                                                             nbatch_test),
                                   last_batch='discard')
 
 
